a09943c96391db0a3c8a0c14891b802e0bb405db
Piwan Samalie <piwan.summerlee@gmail.com>
2021-12-12 02:13:29 +0300

    Added navigation

--- app/src/main/java/com/sample/payoneer/ui/FragmentWelcome.java
+++ app/src/main/java/com/sample/payoneer/ui/FragmentWelcome.java
@@ -1,4 +1,33 @@
 package com.sample.payoneer.ui;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.sample.payoneer.R;
+import com.sample.payoneer.databinding.FragmentWelcomeBinding;
+
 public class FragmentWelcome extends BaseFragment {
+
+    private FragmentWelcomeBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        NavController controller = Navigation.findNavController(view);
+        binding.btnNavigateToPayments.setOnClickListener(view1 -> controller.navigate(R.id.action_fragmentWelcome_to_fragmentList));
+    }
 }
